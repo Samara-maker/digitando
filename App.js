582fb534f1016cb5597de744bd53bd5e3c4d1608
Samara hespanhol <samarahespanhol@gmail.com>
2025-08-29 19:27:04 -0400

    at

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 
-export default function Espelho({ placeholder, label = "Você digitou:" }) {
-  const [texto, setTexto] = useState(''); /**USUARIO, DIGITE AQUI */
-
-  const limpar = () => {
-    setTexto('');
-  };
+export default function Espelho({ placeholder = "Digite algo aqui", label = "Você digitou:" }) {
+  const [texto, setTexto] = useState('11');
 
   return (
     <View>
+      <Text color="blue">Digite no campo abaixo:</Text>
       <TextInput
         placeholder={placeholder}
         value={texto}
         onChangeText={setTexto}
+        color="green"
       />
-      <Text>
+      <Text color="purple">
         {texto ? `${label} ${texto}` : 'Nada digitado ainda..'}
       </Text>
-      <Button title="Limpar" onPress={limpar} />
+      <Button
+        title="Limpar"
+        onPress={() => setTexto('')}
+        color="red"
+      />
     </View>
   );
 }
